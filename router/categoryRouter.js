5cb353335ca205d0d6ddc60c42b85384b697232f
vismayarajeevan <vismayarajeevan15@gmail.com>
2025-03-22 00:35:29 +0530

    category done

--- router/categoryRouter.js
+++ router/categoryRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const categoryController = require("../controller/categoryController");
+const { verifyAdminToken,verifyUserToken } = require("../middleware/verifyToken")
 
-router.post("/", categoryController.addCategory);
-router.get("/", categoryController.getCategories);
-router.get("/:categoryId/foods", categoryController.getCategoryFoods);
-router.put("/category/:categoryId", categoryController.updateCategory);
-router.delete("/category/:categoryId", categoryController.deleteCategory);
+router.post("/",verifyAdminToken, categoryController.addCategory);
+router.get("/",verifyAdminToken, categoryController.getCategories);
+router.get("/:categoryId/foods",verifyAdminToken, categoryController.getCategoryFoods);
+router.put("/category/:categoryId",verifyAdminToken, categoryController.updateCategory);
+router.delete("/category/:categoryId",verifyAdminToken, categoryController.deleteCategory);
 
 module.exports = router;
--- router/foodRouter.js
+++ router/foodRouter.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const foodController = require("../controller/foodController");
 
-router.post("/", foodController.addFood);
-router.get("/", foodController.getAllFood);
-router.get("/category/:categoryId", foodController.getFoodByCategory);
-router.put("/food/:foodId", foodController.updateFood);
-router.delete("/food/:foodId", foodController.deleteFood);
+const { verifyAdminToken,verifyUserToken } = require("../middleware/verifyToken")
+
+router.post("/",verifyAdminToken, foodController.addFood);
+router.get("/",verifyAdminToken,verifyUserToken, foodController.getAllFood);
+router.get("/category/:categoryId",verifyAdminToken, foodController.getFoodByCategory);
+router.put("/food/:foodId",verifyAdminToken, foodController.updateFood);
+router.delete("/food/:foodId",verifyAdminToken, foodController.deleteFood);
 
 module.exports = router;
