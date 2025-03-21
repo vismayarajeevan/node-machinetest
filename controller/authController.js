319ce70d6bf6be1f7319079369d18bf7a8865b2c
vismayarajeevan <vismayarajeevan15@gmail.com>
2025-03-21 15:19:38 +0530

    registration completed

--- controller/authController.js
+++ controller/authController.js
@@ -1,8 +1,77 @@
 const users = require('../model/userModel')
+const bcrypt = require('bcrypt')
+const nodemailer = require('nodemailer')
+
+
+// nodemailer setup
+
+const transport = nodemailer.createTransport({
+    service:"gmail",
+    auth:{
+        user : process.env.NodemailerMail,
+      pass: process.env.NodemailerPassword
+    }
+})
+
 
 
 exports.registerController = async(req,res)=>{
-    console.log("inside register controller");
-    res.status(200).json('register')
+   try {
+
+    const {userName,email,password} = req.body
+
+    if(!userName || !email || !password){
+        res.status(400).json('All fields are required')
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailRegex.test(email)){
+        return res.status(400).json('Invalid email format')
+    }
+
+    const encryptedPassword = await bcrypt.hash(password,10)
+
+    const otp = Math.floor(100000 + Math.random() * 900000);
+
+    const existingUser = await users.findOne({email})
+
+    if(existingUser){
+        if(existingUser.verified){
+            res.status(400).json({message:'User already registered and verified with this email'})
+
+        }else{
+          existingUser.otp =otp
+          existingUser.password = encryptedPassword
+          await existingUser.save()
+        }
+    }else{
+        const newUser = new users({
+            userName,
+            email,
+            password:encryptedPassword,
+            otp
+        })
+
+        await newUser.save()
+    }
+
+    // sent otp
+
+    await transport.sendMail({
+        from:process.env.NodemailerMail,
+      to: email,
+      subject:'OTP Verification',
+      text:`Your OTP for verification is: ${otp}`,
+    })
+
+    res.status(200).json({message:'OTP sent successfully'})
+
+
+
+   } catch (error) {
+    console.log("error during registration",error);
+    
+    res.status(500).json({ message: 'Failed to send OTP', error: error.message })
+   }
     
 }
